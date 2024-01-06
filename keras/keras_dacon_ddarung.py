0dc4579c7fe647786bb6afd30d50514fd1c32fd7
AIKONS2024 <aikong2024@gmail.com>
2024-01-06 20:53:28 +0900

    -fixed

--- keras/keras_dacon_ddarung.py
+++ keras/keras_dacon_ddarung.py
@@ -72,95 +72,133 @@ pm2.5 :평균
 
 '''
 
-
-
-# 1. 데이터 불러오기
 import pandas as pd
 path = '/Users/kongseon-eui/Documents/Workspace/AI_Project/_data/'
 train_csv = pd.read_csv(path + 'train.csv')
 test_csv = pd.read_csv(path + 'test.csv')
 submission_csv = pd.read_csv(path + 'submission.csv')
 
-# 2. 데이터 정규화
-#데이터 모양
-# print(train_csv.shape)  # (1459, 11)
-# print(test_csv.shape)  # (715, 10)
-# print(submission_csv.shape)  # (715, 2)
+class Dacon_ddarung:
+    # 1. 데이터 불러오기
 
-#결측치 제거
+    from sklearn.model_selection import train_test_split
+    from keras.models import Sequential
+    from keras.layers import Dense
+    from sklearn.metrics import r2_score
+    import random
+    
+    #save submission file
+    def save_submission(file):
+        import time as tm
+        ltm = tm.localtime(tm.time())
+        save_time = f"{ltm.tm_year}{ltm.tm_mon}{ltm.tm_mday}{ltm.tm_hour}{ltm.tm_min}{ltm.tm_sec}" 
+        file.to_csv(path + f"submission_{save_time}.csv", index=False)
+        
+    #그래프 확인 
+    def show_graph(realData,predictData):
+        import matplotlib.pyplot as plt
+        plt.scatter(realData, predictData)
+        # plt.plot(y_test, color = 'red')  # Plotting actual values
+        plt.show()
+        
+    def save_file(value):
+        df = pd.DataFrame({'random_state' : [value[0]], 'r2' : [value[1]]}) 
+        # dfs = pd.DataFrame(data)
+        df.to_csv(path + "random_state.csv", mode='a', index=True)
+        
+        
+    # 2. 데이터 정규화
+    #데이터 모양
+    # print(train_csv.shape)  # (1459, 11)
+    # print(test_csv.shape)  # (715, 10)
+    # print(submission_csv.shape)  # (715, 2)
 
-#train
-train_csv['hour_bef_precipitation'].fillna(value= 0.0, inplace=True)
-# train_csv.fillna(test_csv.mean(), inplace= True)
-train_csv.fillna(train_csv.interpolate(method='values'), inplace=True)
+    #결측치 제거
 
-#test
-test_csv['hour_bef_precipitation'].fillna(value= 0.0 , inplace=True)
-# test_csv.fillna(test_csv.mean(), inplace=True)
-test_csv.fillna(test_csv.interpolate(method='values'), inplace=True)
+    #train
+    train_csv['hour_bef_precipitation'].fillna(value= train_csv['hour_bef_precipitation'].mean() , inplace=True)
+    # train_csv.fillna(test_csv.mean(), inplace= True)
+    train_csv.fillna(train_csv.interpolate(method='values'), inplace=True)
 
+    #test
+    test_csv['hour_bef_precipitation'].fillna(value= train_csv['hour_bef_precipitation'].mean() , inplace=True)
+    # test_csv.fillna(test_csv.mean(), inplace=True)
+    test_csv.fillna(test_csv.interpolate(method='values'), inplace=True)
+    
 
+    # print(train_csv.isna().sum())
+    # print(test_csv.isna().sum())
 
+    #데이터 x,y 자름
+    x = train_csv.drop('count', axis= 1)
+    # x = train_csv.drop(columns='count')
+    y = train_csv['count']
+    
+    
+       #영향이 없는 컬럼 제거
+    x = x.drop('hour_bef_visibility', axis=1)
+    # x = x.drop('hour_bef_ozone', axis=1)
+    # x = x.drop('hour_bef_precipitation', axis=1)
+    # x = x.drop('hour_bef_windspeed', axis=1)
+    x = x.drop('hour_bef_pm10', axis=1)
+    x = x.drop('hour_bef_pm2.5', axis=1)
+    
+    test_t = test_csv
+    test_t = test_t.drop('hour_bef_visibility', axis=1)
+    # test_t = test_t.drop('hour_bef_ozone', axis=1)
+    # test_t = test_t.drop('hour_bef_precipitation', axis=1)
+    # test_t = test_t.drop('hour_bef_windspeed', axis=1)
+    test_t = test_t.drop('hour_bef_pm10', axis=1)
+    test_t = test_t.drop('hour_bef_pm2.5', axis=1)
+    
 
-# print(train_csv.isna().sum())
-# print(test_csv.isna().sum())
+    # print(x.shape)#(1459, 10)
+    # print(y.shape)#(1459,)
 
-#데이터 x,y 자름
-x = train_csv.drop('count', axis= 1)
-# x = train_csv.drop(columns='count')
-y = train_csv['count']
+    # best_random_state = 0
+    # import random
+    # random_random_state = random.randint(0,4290000000)
+    # 3. 데이터 셔플 및 분리
 
-# print(x.shape)#(1459, 10)
-# print(y.shape)#(1459,)
+    
+    random_random_state = 0
+    # while(1) :
+    random_random_state = random.randint(0,1000)
+    x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=0.75, random_state=0)
 
-# best_random_state = 0
-# import random
-# random_random_state = random.randint(0,4290000000)
-# 3. 데이터 셔플 및 분리
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=0.75, random_state=6131483)
+    ##################################################데이터 전처리
+    
+    # 모델구성
+    model = Sequential()
+    model.add(Dense(1, input_dim = len(x.columns)))
+    model.add(Dense(1))
+    model.add(Dense(1))
+    model.add(Dense(1))
 
-##################################################데이터 전처리
+    
 
-from keras.models import Sequential
-from keras.layers import Dense
+    #컴파일, 훈련
+    model.compile(loss='mse', optimizer='adam')
+    model.fit(x_train, y_train, epochs=200, batch_size=10) 
+    
+    #평가 ,예측
+    loss = model.evaluate(x_test, y_test)
 
-# 모델구성
-model = Sequential()
-model.add(Dense(32, input_dim = 10))
-model.add(Dense(64))
-model.add(Dense(128))
-model.add(Dense(1))
+    y_predict = model.predict(x_test)
+    r2 = r2_score(y_test, y_predict)
+    
+    submit = model.predict([test_t])
+    
+    print('loss : ', loss)
+    print('r2 : ', r2)   
+    print('로스 차이:', )
+    
+    #sumission.csv 파일 생성
+    submission_csv['count'] = submit
+    save_submission(submission_csv)
+    save_file([random_random_state, r2])
+    
+    show_graph(y_test, y_predict)
+    
+            
 
-#컴파일, 훈련
-model.compile(loss='mse', optimizer='adam')
-model.fit(x_train, y_train, epochs=50, batch_size=20) #epo : 10, 50, 100  /// bat : 10, 20, 40, 60 ,80
-
-#평가 ,예측
-loss = model.evaluate(x_test, y_test)
-
-from sklearn.metrics import r2_score
-y_predict = model.predict(x_test)
-r2 = r2_score(y_test, y_predict)
-submit = model.predict([test_csv])
-print(submit.shape)
-print(y_predict.shape)
-# r2_2 = r2_score(y_test, submit[:365])/
-
-print('loss : ', loss)
-print('r2 : ', r2)
-# print('r2_2 : ', r2_2)
-
-#sumission.csv 파일 생성
-submission_csv['count'] = submit
-
-import time as tm
-ltm = tm.localtime(tm.time())
-save_time = f"{ltm.tm_year}{ltm.tm_mon}{ltm.tm_mday}{ltm.tm_hour}{ltm.tm_min}{ltm.tm_sec}" 
-submission_csv.to_csv(path + f"submission_{save_time}.csv", index=False)
-
-#그래프 확인
-import matplotlib.pyplot as plt
-plt.scatter(y_test, y_predict)
-# plt.plot(y_test, color = 'red')  # Plotting actual values
-plt.show()

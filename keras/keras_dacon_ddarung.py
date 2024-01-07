5100c38687c01258ad164c84ae8feb83d7ebdf14
AIKONS2024 <aikong2024@gmail.com>
2024-01-07 12:44:20 +0900

    -fix auto submit  func

--- keras/keras_dacon_ddarung.py
+++ keras/keras_dacon_ddarung.py
@@ -71,12 +71,28 @@ pm2.5 :평균
 
 
 '''
+import time as tm
+def get_save_time():
+    ltm = tm.localtime(tm.time())
+    save_time = f"{ltm.tm_year}{ltm.tm_mon}{ltm.tm_mday}{ltm.tm_hour}{ltm.tm_min}{ltm.tm_sec}" 
+    return save_time
 
+def auto_submit(file_path):
+    from dacon_submit_api import dacon_submit_api
+    result = dacon_submit_api.post_submission_file(
+        file_path,
+        'fe659a916a1e7c90f38f21a116396e67921dc583af5139016d88b9d1ca49ae6b',
+        '235576',
+        '옹선응',
+        '자동 제출'
+    )
+    
 import pandas as pd
 path = '/Users/kongseon-eui/Documents/Workspace/AI_Project/_data/'
 train_csv = pd.read_csv(path + 'train.csv')
 test_csv = pd.read_csv(path + 'test.csv')
 submission_csv = pd.read_csv(path + 'submission.csv')
+headerFlag = True
 
 class Dacon_ddarung:
     # 1. 데이터 불러오기
@@ -88,11 +104,12 @@ class Dacon_ddarung:
     import random
     
     #save submission file
-    def save_submission(file):
-        import time as tm
-        ltm = tm.localtime(tm.time())
-        save_time = f"{ltm.tm_year}{ltm.tm_mon}{ltm.tm_mday}{ltm.tm_hour}{ltm.tm_min}{ltm.tm_sec}" 
-        file.to_csv(path + f"submission_{save_time}.csv", index=False)
+    
+    
+    def save_submission():
+        file_path = path + f"submission_{get_save_time()}.csv"
+        submission_csv.to_csv(file_path, index=False)
+        # auto_submit(file_path)
         
     #그래프 확인 
     def show_graph(realData,predictData):
@@ -102,28 +119,33 @@ class Dacon_ddarung:
         plt.show()
         
     def save_file(value):
-        df = pd.DataFrame({'random_state' : [value[0]], 'epoch' : [value[1]], 'train_size' : [value[2]], 'batch_size' : [value[3]],  'gap' : [value[4]], 'loss' : [value[5]]}) 
-        # dfs = pd.DataFrame(data)
-        df.to_csv(path + "random_state.csv", mode='a', index=True)
+        global headerFlag
+        df_new = pd.DataFrame({'random_state' : [str(value[0])], 'epoch' : [str(value[1])], 'train_size' : [str(value[2])], 'batch_size' : [str(value[3])],  'gap' : [str(value[4])], 'loss' : [str(value[5])], 'file' : [get_save_time()]}) 
+        df_new.to_csv(path + "random_state.csv", mode= 'a', header=headerFlag)
+        headerFlag = False
         
         
     # 2. 데이터 정규화
     #데이터 모양
     # print(train_csv.shape)  # (1459, 11)
-    # print(test_csv.shape)  # (715, 10)
+    # print(test_csv.shape)  # (715, 10)s
     # print(submission_csv.shape)  # (715, 2)
 
     #결측치 제거
+    #결측치가 있는 행 제거
+    # train_csv.dropna(inplace=True)
+    # test_csv.dropna(inplace=True)
+    
 
     #train
-    train_csv['hour_bef_precipitation'].fillna(value= train_csv['hour_bef_precipitation'].mean() , inplace=True)
-    # train_csv.fillna(test_csv.mean(), inplace= True)
-    train_csv.fillna(train_csv.interpolate(method='values'), inplace=True)
+    train_csv['hour_bef_precipitation'].fillna(value= 0.0 , inplace=True)
+    train_csv.fillna(test_csv.mean(), inplace= True)
+    # train_csv.fillna(train_csv.interpolate(method='values'), inplace=True)
 
     #test
-    test_csv['hour_bef_precipitation'].fillna(value= train_csv['hour_bef_precipitation'].mean() , inplace=True)
-    # test_csv.fillna(test_csv.mean(), inplace=True)
-    test_csv.fillna(test_csv.interpolate(method='values'), inplace=True)
+    test_csv['hour_bef_precipitation'].fillna(value= 0.0 , inplace=True)
+    test_csv.fillna(test_csv.mean(), inplace=True)
+    # test_csv.fillna(test_csv.interpolate(method='values'), inplace=True)
     
 
     # print(train_csv.isna().sum())
@@ -136,20 +158,20 @@ class Dacon_ddarung:
     
     
        #영향이 없는 컬럼 제거
-    x = x.drop('hour_bef_visibility', axis=1)
+    # x = x.drop('hour_bef_visibility', axis=1)
     # x = x.drop('hour_bef_ozone', axis=1)
     # x = x.drop('hour_bef_precipitation', axis=1)
     # x = x.drop('hour_bef_windspeed', axis=1)
-    x = x.drop('hour_bef_pm10', axis=1)
-    x = x.drop('hour_bef_pm2.5', axis=1)
+    # x = x.drop('hour_bef_pm10', axis=1)
+    # x = x.drop('hour_bef_pm2.5', axis=1)
     
     test_t = test_csv
-    test_t = test_t.drop('hour_bef_visibility', axis=1)
+    # test_t = test_t.drop('hour_bef_visibility', axis=1)
     # test_t = test_t.drop('hour_bef_ozone', axis=1)
     # test_t = test_t.drop('hour_bef_precipitation', axis=1)
     # test_t = test_t.drop('hour_bef_windspeed', axis=1)
-    test_t = test_t.drop('hour_bef_pm10', axis=1)
-    test_t = test_t.drop('hour_bef_pm2.5', axis=1)
+    # test_t = test_t.drop('hour_bef_pm10', axis=1)
+    # test_t = test_t.drop('hour_bef_pm2.5', axis=1)
     
 
     # print(x.shape)#(1459, 10)
@@ -165,11 +187,16 @@ class Dacon_ddarung:
     random_epoch = 0
     random_batch_size = 0
     random_train_size = 0.0
-    while(1) :
-        random_random_state = random.randint(0,10000000)
-        random_epoch = random.randint(50,500)
-        random_batch_size = random.randint(10,100)
-        random_train_size = round(random.uniform(0.6,0.9),2)
+    for i in range(0, 101) :
+        random_random_state = i
+        # random_epoch = random.randint(2,10) * 50
+        # random_batch_size = random.randint(1,50) * 10 
+        # random_train_size = round(random.uniform(0.60,0.90),2)
+        
+        random_random_state = 341
+        random_epoch = 300
+        random_batch_size = 160
+        random_train_size = 0.9
         
         x_train, x_test, y_train, y_test = train_test_split(x,y, shuffle=True, train_size=random_train_size, random_state=random_random_state)
 
@@ -177,11 +204,8 @@ class Dacon_ddarung:
         
         # 모델구성
         model = Sequential()
-        model.add(Dense(2, input_dim = len(x.columns)))
-        model.add(Dense(3))
-        model.add(Dense(4))
-        model.add(Dense(3))
-        model.add(Dense(2))
+        model.add(Dense(64, input_dim = len(x.columns)))
+        model.add(Dense(32))
         model.add(Dense(1))
         
 
@@ -202,12 +226,11 @@ class Dacon_ddarung:
         gap_of_loss = abs(model.evaluate(x_train,y_train) - loss)
         print('로스 차이:', gap_of_loss)
         
-        #sumission.csv 파일 생성
-        if loss < 2700 and gap_of_loss < 100:
+        save_file([random_random_state, random_epoch, random_train_size, random_batch_size, gap_of_loss, loss])
+        if loss < 1700 :
             submission_csv['count'] = submit
-            save_submission(submission_csv)
-            save_file([random_random_state, random_epoch, random_train_size, random_batch_size, gap_of_loss, loss])
-        
+            save_submission()
+            
         
     # show_graph(y_test, y_predict)
     

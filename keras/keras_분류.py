5d18c22e5a06be022833e99296dc219a7afd52aa
AIKONS2024 <aikong2024@gmail.com>
2024-01-11 23:03:08 +0900

    -add

---
+++ keras/keras_분류.py
@@ -0,0 +1,7 @@
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
